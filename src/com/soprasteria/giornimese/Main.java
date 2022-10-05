eebb0f71042a4ff14f0fdb31d1642c47d71d711a
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 16:23:23 +0200

    inserimento nome del mese

--- src/com/soprasteria/giornimese/Main.java
+++ src/com/soprasteria/giornimese/Main.java
@@ -1,10 +1,17 @@
 package com.soprasteria.giornimese;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String nomeMese;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Inserisci il nome del mese: ");
+		nomeMese = s.nextLine();
 	}
 
 }
