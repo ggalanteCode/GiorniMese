11bdcce1f7da4ccf65106b23d7c7494818038301
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 17:12:25 +0200

    semplificazione dell' if/else

--- src/com/soprasteria/giornimese/Main.java
+++ src/com/soprasteria/giornimese/Main.java
@@ -13,30 +13,21 @@ public class Main {
 		System.out.print("Inserisci il nome del mese: ");
 		nomeMese = s.nextLine();
 		
-		if(nomeMese.equalsIgnoreCase("gennaio"))
+		if(nomeMese.equalsIgnoreCase("gennaio") ||
+				nomeMese.equalsIgnoreCase("marzo") ||
+				nomeMese.equalsIgnoreCase("maggio") ||
+				nomeMese.equalsIgnoreCase("luglio") ||
+				nomeMese.equalsIgnoreCase("agosto") ||
+				nomeMese.equalsIgnoreCase("ottobre") ||
+				nomeMese.equalsIgnoreCase("dicembre"))
 			System.out.println("Numero giorni: " + 31);
 		else if(nomeMese.equalsIgnoreCase("febbraio"))
 			System.out.println("Numero giorni: " + 28);
-		else if(nomeMese.equalsIgnoreCase("marzo"))
-			System.out.println("Numero giorni: " + 31);
-		else if(nomeMese.equalsIgnoreCase("aprile"))
+		else if(nomeMese.equalsIgnoreCase("aprile") ||
+				nomeMese.equalsIgnoreCase("giugno") ||
+				nomeMese.equalsIgnoreCase("settembre") ||
+				nomeMese.equalsIgnoreCase("novembre"))
 			System.out.println("Numero giorni: " + 30);
-		else if(nomeMese.equalsIgnoreCase("maggio"))
-			System.out.println("Numero giorni: " + 31);
-		else if(nomeMese.equalsIgnoreCase("giugno"))
-			System.out.println("Numero giorni: " + 30);
-		else if(nomeMese.equalsIgnoreCase("luglio"))
-			System.out.println("Numero giorni: " + 31);
-		else if(nomeMese.equalsIgnoreCase("agosto"))
-			System.out.println("Numero giorni: " + 31);
-		else if(nomeMese.equalsIgnoreCase("settembre"))
-			System.out.println("Numero giorni: " + 30);
-		else if(nomeMese.equalsIgnoreCase("ottobre"))
-			System.out.println("Numero giorni: " + 31);
-		else if(nomeMese.equalsIgnoreCase("novembre"))
-			System.out.println("Numero giorni: " + 30);
-		else if(nomeMese.equalsIgnoreCase("dicembre"))
-			System.out.println("Numero giorni: " + 31);
 	}
 
 }
